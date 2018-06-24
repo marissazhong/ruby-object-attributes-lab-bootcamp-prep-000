0278e6957d50962eea22d208be725f44f67516d0
Marissa Zhong <mwz2101@columbia.edu>
2018-06-24 01:04:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,12 @@ class Dog
   def name
     @name
   end
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
   def breed
     @breed
   end
 
-  def breed=(new_breed)
-    @breed = new_breed
-  end
+
 end
